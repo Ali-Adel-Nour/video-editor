61b2be857844d46e90c3c5e3f82cbed620907f46
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-11-03 20:27:50 +0200

    Download Routes

--- src/controllers/video.js
+++ src/controllers/video.js
@@ -6,11 +6,14 @@ const util = require("../../lib/util");
 const DB = require("../DB");
 const FF = require("../../lib/FF");
 
+// Return the list of all the videos that a logged in user has uploaded
 const getVideos = (req, res, handleErr) => {
-  const videos = DB.videos.filter((video)=>{
-    return video.userId === req.userId
-  })
-  res.status(200).json(videos)
+  DB.update();
+  const videos = DB.videos.filter((video) => {
+    return video.userId === req.userId;
+  });
+
+  res.status(200).json(videos);
 };
 
 // Upload a video file
@@ -68,59 +71,117 @@ const uploadVideo = async (req, res, handleErr) => {
   }
 };
 
+// Extract the audio for a video file (can only be done once per video)
+const extractAudio = async (req, res, handleErr) => {
+  const videoId = req.params.get("videoId");
 
-const getVideoAsset = async (req,res,handleErr)=>{
-  const videoId = req.params.get("videoId")
-  const type = req.params.get("type")
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
 
+  if (video.extractedAudio) {
+    return handleErr({
+      status: 400,
+      message: "The audio has already been extracted for this video.",
+    });
+  }
 
-  DB.update()
-  const video = DB.videos.find((video)=>video.videoId === videoId)
+  try {
+    const originalVideoPath = `./storage/${videoId}/original.${video.extension}`;
+    const targetAudioPath = `./storage/${videoId}/audio.aac`;
 
-  if(!video) return handleErr({
-    status:404,
-    message:"Video not found"
-  })
+    await FF.extractAudio(originalVideoPath, targetAudioPath);
+
+    video.extractedAudio = true;
+    DB.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "The audio was extracted successfully!",
+    });
+  } catch (e) {
+    util.deleteFile(targetAudioPath);
+    return handleErr(e);
+  }
+};
+
+// Return a video asset to the client
+const getVideoAsset = async (req, res, handleErr) => {
+  const videoId = req.params.get("videoId");
+  const type = req.params.get("type"); // thumbnail, original, audio, resize
+
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
+
+  if (!video) {
+    return handleErr({
+      status: 404,
+      message: "Video not found!",
+    });
+  }
 
   let file;
   let mimeType;
+  let filename; // the final file name for the download (including the extension)
 
-  switch(type){
+  switch (type) {
     case "thumbnail":
-       file = await fs.open(`./storage/${videoId}/thumbnail.jpg`,"r")
-       mimeType = "image/jpeg"
-       break;
+      file = await fs.open(`./storage/${videoId}/thumbnail.jpg`, "r");
+      mimeType = "image/jpeg";
+      break;
+    case "audio":
+      file = await fs.open(`./storage/${videoId}/audio.aac`, "r");
+      mimeType = "audio/aac";
+      filename = `${video.name}-audio.aac`;
+      break;
+    case "resize":
+      const dimensions = req.params.get("dimensions");
+      file = await fs.open(
+        `./storage/${videoId}/${dimensions}.${video.extension}`,
+        "r"
+      );
+      mimeType = "video/mp4"; // Not a good practice! Videos are not always MP4
+      filename = `${video.name}-${dimensions}.${video.extension}`;
+      break;
+    case "original":
+      file = await fs.open(
+        `./storage/${videoId}/original.${video.extension}`,
+        "r"
+      );
+      mimeType = "video/mp4"; // Not a good practice! Videos are not always MP4
+      filename = `${video.name}.${video.extension}`;
+      break;
   }
 
-  //grab the file size
-try{
-  const stat = await file.stat()
+  try {
+    // Grab the file size
+    const stat = await file.stat();
 
-  const fileStream = file.createReadStream()
+    const fileStream = file.createReadStream();
 
-  res.setHeader("Content-Type",mimeType)
-  res.setHeader("Content-Length",stat.size)
-  res.status(200)
+    if (type !== "thumbnail") {
+      // Set a header to prompt for download
+      res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
+    }
 
-  await pipeline(fileStream, res);
+    // Set the Content-Type header based on the file type
+    res.setHeader("Content-Type", mimeType);
+    // Set the Content-Length to the size of the file
+    res.setHeader("Content-Length", stat.size);
 
-} catch (e) {
-  console.error(e);
-  handleErr({
-    status: 500,
-    message: "Internal server error"
-  });
-} finally {
-  // Ensure the file handle is closed
-  if (file) {
-    await file.close();
+    res.status(200);
+
+    await pipeline(fileStream, res);
+    file.close();
+  } catch (e) {
+    console.log(e);
   }
-}
 };
+
 const controller = {
   getVideos,
   uploadVideo,
-  getVideoAsset
+  extractAudio,
+  getVideoAsset,
 };
 
 module.exports = controller;
